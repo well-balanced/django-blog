8eb39c6d9a0752a1d0d275e81fbb9ae6f50a4388
cosmian <softkey95@gmail.com>
2019-10-17 00:24:59 +0900

    base.html body title edited.

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from django.utils import timezone
 
 admin.site.register(Post)
 # Register your models here.
--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    url(r'^draft/$', views.post_draft_list, name='post_draft_list'),
 ]
